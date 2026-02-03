bc3ec69c0120048088d3e2d7ba8574e924bac06c
Lakshya <lakshya@Lakshyas-MacBook-Air.local>
2026-02-03 13:47:55 +0530

    update on api

--- backend/prompts.py
+++ backend/prompts.py
@@ -4,7 +4,7 @@ Answer ONLY from provided context.
 If information is not available, say:
 "Good question — this actually depends on a few details.
 Instead of guessing, I can connect you with the right person who can guide you properly.
-Shall I arrange a quick call? if says yes then ask for the number and name and then save it in the appointments.json file"
+Shall I arrange a quick call? if says yes then then say write book appointment to start the procedure of booking"
 
 Never give medical advice.
 Only explain services and process.
